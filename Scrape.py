51972a2ed5c201c229e5382f174c888cac48b06d
Varun Nair <54781358+VarunNair22@users.noreply.github.com>
2021-11-06 17:07:20 -0400

    changes

--- Scrape.py
+++ Scrape.py
@@ -1,4 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
 class Scrape():
     def __init__(self, url, name):
         self.url = url
         self.name = name
+        self.page = ''
+
+    def scrapeUrl(self):
+        self.page = requests.get(self.url)
--- scrape-overbuff.py
+++ scrape-overbuff.py
@@ -1,11 +1,32 @@
 import requests
+from fake_useragent import UserAgent
 from Scrape import Scrape
+from bs4 import BeautifulSoup
+
 
 class ScrapeOverbuff(Scrape):
     def __init__(self, url, name):
         super().__init__(url, name)
 
+    def scrapeUrl(self):
+        super().scrapeUrl()
+
 if __name__ == '__main__':
-    scrape = ScrapeOverbuff('url_here', 'DaddyDerek')
+    scrape = ScrapeOverbuff('https://www.overbuff.com/heroes', 'arjay12345#1967')
     print(scrape.name)
+    scrape.scrapeUrl()
+    print(scrape.page.text)
+
+    ua = UserAgent()
+
+    def lovely_soup(u):
+        r = requests.get(u, headers={'User-Agent': ua.chrome})
+        print(r.text)
+        return BeautifulSoup(r.text, 'lxml')
+
+    url = 'https://www.overbuff.com'
+    soup = lovely_soup(url)
+
+    test = soup.find('div', {'class': 'layout-notification'}).text
+    print(test.strip())
 
